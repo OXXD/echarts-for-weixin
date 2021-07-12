7bbcd50492a26ce14121675216fee7caed33f7cf
oxxd <oxxxxd@gmail.com>
2021-07-12 10:40:22 +0800

    chore: 增加小程序引用示例

--- tools/demo/pages/index/index.js
+++ tools/demo/pages/index/index.js
@@ -1,1 +1,13 @@
-Page({})
+Page({
+  onClickBar: function () {
+    wx.navigateTo({
+      url: '/pages/bar/index'
+    })
+  },
+  /** TODO: */
+  onClickManualLoad: function () {
+    wx.navigateTo({
+      url: '/pages/bar/index'
+    })
+  }
+})
